e19ba7cf1b931c765127d9bbfe5098e9084cff55
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-02-18 12:21:40 +0200

    Implemented dodge

---
+++ src/multiply.py
@@ -0,0 +1,9 @@
+# Matthew Harris
+# 241ADB166
+# Homework Assignment 1
+# Multiply blending mode
+import numpy as np
+
+def multiply(img1, img2):
+    result = img1.astype(np.float32) * img2.astype(np.float32) / 255.0
+    result = np.clip(result, 0, 255).astype(np.uint8)
