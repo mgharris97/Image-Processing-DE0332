605a4a775b04fa0baeca09a602b0b8afc29a254c
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-02-19 00:51:07 +0200

    Assignment completed

--- src/multiply.py
+++ src/multiply.py
@@ -2,8 +2,12 @@
 # 241ADB166
 # Homework Assignment 1
 # Multiply blending mode
+
 import numpy as np
 
 def multiply(img1, img2):
-    result = img1.astype(np.float32) * img2.astype(np.float32) / 255.0
+    A = img1.astype(np.float32)
+    B = img2.astype(np.float32)
+    result = A * B / 255.0
     result = np.clip(result, 0, 255).astype(np.uint8)
+    return result
